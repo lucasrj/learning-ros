ba789b9e9ce18b60c7a6113cf09d561984a2acca
lucasrj <12768520+lucasrj@users.noreply.github.com>
2021-10-28 22:00:20 +0200

    played around

--- camara test/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++ camara test/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(beginner_tutorials_MESSAGE_FILES "/home/lucasrj/ros/test/src/beginner_tutorials/msg/num.msg")
-set(beginner_tutorials_SERVICE_FILES "/home/lucasrj/ros/test/src/beginner_tutorials/srv/AddTwoInts.srv")
+set(beginner_tutorials_MESSAGE_FILES "/home/lucasrj/github/learning-ros/camara test/src/beginner_tutorials/msg/num.msg")
+set(beginner_tutorials_SERVICE_FILES "/home/lucasrj/github/learning-ros/camara test/src/beginner_tutorials/srv/AddTwoInts.srv")
